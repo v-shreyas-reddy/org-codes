afa20c0083eb63e5f36c97a33210395b6cac1200
v-shreyas-reddy <shreyasreddyv0@gmail.com>
2024-08-10 23:41:54 +0530

    added acc and opp and toast event and refactored

--- force-app/main/default/lwc/formResource/formResource.js
+++ force-app/main/default/lwc/formResource/formResource.js
@@ -1,17 +1,64 @@
-import { LightningElement, track, api } from "lwc";
+import { LightningElement, track, api, wire } from "lwc";
+import { ShowToastEvent } from "lightning/platformShowToastEvent";
 import getMultiplePicklistValues from "@salesforce/apex/PicklistController.getMultiplePicklistValues";
 
 import insertResourceRecord from "@salesforce/apex/ResourceController.insertResource";
+import ACCOUNTS from "@salesforce/apex/AccountController.getAllAccounts";
+import OPPORTUNITIES from "@salesforce/apex/OpportunityController.getAllOpps";
 
 export default class FormResource extends LightningElement {
   @track picklistOptionsMap = {};
   @track resource = {};
+  oppOptions = [];
+  accountOptions = [];
+
+  //Toast event function
+  showToast(title, message, variant) {
+    this.dispatchEvent(
+      new ShowToastEvent({
+        title,
+        message,
+        variant
+      })
+    );
+  }
+
+  //Opportunity lookup method
+  @wire(OPPORTUNITIES)
+  wiredOpps({ error, data }) {
+    if (data) {
+      this.oppOptions = data.map((opportunity) => {
+        return {
+          label: opportunity.Name,
+          value: opportunity.Id
+        };
+      });
+    } else if (error) {
+      console.log("Error: " + JSON.stringify(error));
+    }
+  }
+
+  //Account lookup method
+  @wire(ACCOUNTS)
+  wiredAccounts({ error, data }) {
+    if (data) {
+      this.accountOptions = data.map((account) => {
+        return {
+          label: account.Name,
+          value: account.Id
+        };
+      });
+    } else if (error) {
+      console.log("Error: " + JSON.stringify(error));
+    }
+  }
 
   connectedCallback() {
     this.fetchMultiplePicklistValues();
   }
 
   fetchMultiplePicklistValues() {
+    const objectApiName = "Resource__c";
     const fields = [
       "Client_Interview_Required__c",
       "Department__c",
@@ -28,7 +75,7 @@ export default class FormResource extends LightningElement {
       "Billability__c",
       "Shift_Timings__c"
     ];
-    getMultiplePicklistValues({ objectName: "Resource__c", fieldNames: fields })
+    getMultiplePicklistValues({ objectName: objectApiName, fieldNames: fields })
       .then((result) => {
         this.picklistOptionsMap = {};
         // eslint-disable-next-line guard-for-in
@@ -52,14 +99,12 @@ export default class FormResource extends LightningElement {
   handlePicklistChange(event) {
     const field = event.target.dataset.field;
     this.resource[field] = event.detail.value;
-    // console.log("Field:--- ", field, "data:---", this.resource[field]);
+    // console.log("Field:--- " + field + " data:--- " + this.resource[field]);
   }
 
   appdata;
   @api resourceId; // property of resourceId after insertion
   handleSaveClick() {
-    // console.log("total data----" + this.resource);
-    // console.log("Final resource data: " + JSON.stringify(this.resource));
     this.appdata = JSON.stringify(this.resource);
     insertResourceRecord({ resourceData: this.appdata })
       .then((result) => {
@@ -67,12 +112,12 @@ export default class FormResource extends LightningElement {
         // eslint-disable-next-line @lwc/lwc/no-api-reassignments
         this.resourceId = result.Id;
         console.log("Resource record ID=====: " + this.resourceId);
-        // this.showToast("Success", "Resource saved successfully", "success");
+        this.showToast("Success", "Resource saved successfully", "success");
         // this.handleApproval();
       })
       .catch((error) => {
         console.log("Error while Saving=====> " + JSON.stringify(error));
-        // this.showToast("Resource Not Saved", error.body.message, "error");
+        this.showToast("Resource Not Saved", error.body.message, "error");
       });
   }
 }
